a3cf7aba59eec6df295b167e1ed0ae4989a21070
Olivia <olivia@macbook-pro.myfiosgateway.com>
2020-09-11 13:46:53 -0400

    Sprint Challenge_Intro to React First Commitment

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,33 @@
 // Write your Character component here
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Character = () => {
+    const [character, setCharacter] = useState([])
+    useEffect(() =>{
+      axios.get("https://swapi.dev/api/people")
+      .then((response) => {
+        setCharacter(response.data.results)
+      })
+      .catch((error) =>{
+        console.log(error)
+      })
+    }, [])
+    return(
+        <div>
+            {
+                character.length && character.map(character => {
+                    return <div>
+                        <h3>{character.name}</h3>
+                        <div>{character.height}</div>
+                        <div>{character.birthYear}</div>
+                    </div>
+                })
+            }
+  
+        </div>
+    )
+}
+
+export default Character;
